975b8ddedb5de26a00475c9c75c64f2416c347dd
JusticeEli <justelonelin@gmail.com>
2026-09-12 20:40:35 +0300

    feat: increase debounce

--- src/util.ts
+++ src/util.ts
@@ -52,7 +52,7 @@ const setListenersForGlobalElements = (callback: () => void) => {
 
         timeout = window.setTimeout(() => {
             callback();
-        }, 500); // wait 500ms after mutations settle
+        }, 2000); // wait 500ms after mutations settle
     });
 
     observer.observe(document.body, {
